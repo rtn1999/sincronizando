39feab73a7c56376ee59cd21dd0afed186ee50b3
raul <rtrujillo2599@gmail.com>
2023-05-04 11:53:51 +0200

    me cago en to

--- nav.php
+++ nav.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav>
   <ol>
     <li><a href="#">Home</li>
@@ -7,13 +6,3 @@
     <li><a href="#">Contacto</li>
   </ol>
 </nav>
-=======
-<header>
-  <ol>
-    <li><a href="#">Home</li>
-    <li><a href="#">Quienes somos</li>
-    <li><a href="#">Nosotros</li>
-    <li><a href="#">Contacto</li>
-  </ol>
-</header>
->>>>>>> d08a2792c806497eaf4f95fd2f1906a4ab10a6cf
